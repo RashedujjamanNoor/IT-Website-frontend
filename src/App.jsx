54f1a1ff90a3b92fba19bf23652584416e67d8d3
RashedujjamanNoor <noorrk042@gmail.com>
2024-08-18 22:46:24 +0600

    create project and all pages route

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Service from "./pages/Service";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
