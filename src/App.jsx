2a183ab38d3b707566bc8024c877d60c9b130c8e
RashedujjamanNoor <noorrk042@gmail.com>
2024-08-18 23:18:19 +0600

    create navbar ui/ux

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Register from "./pages/Register";
 import Service from "./pages/Service";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="bg-gray-800 text-blue-400 font-bold">
+      <header className="flex justify-between items-center py-3 mx-[5%]">
+        <div>
+          <h1 className="text-2xl">Noor.</h1>
+        </div>
+        <nav>
+          <ul className="hidden md:flex justify-between items-center gap-4">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+            <li>
+              <NavLink to="/service">Service</NavLink>
+            </li>
+            <li>
+              <NavLink to="/register">Register</NavLink>
+            </li>
+            <li>
+              <NavLink to="/login">Login</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
